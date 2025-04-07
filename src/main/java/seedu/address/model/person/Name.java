b43ff4682bd6a8c0a82d3ce8c0fa3f33be362d0b
Dexter Kwxn <dexter.kwxn@gmail.com>
2025-04-07 23:28:16 +0800

    Update src/main/java/seedu/address/model/person/Name.java
    Co-authored-by: Zhao Wei Liew <zhaoweiliew@gmail.com>

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -10,8 +10,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters and spaces, and it should not be blank."
-                    + "\nSpecial characters like -, \', and / are supported.";
+            "Name must not be blank, and it must contain only alphanumeric characters, spaces,"
+                    + " apostrophes, slashes, and dashes.";
 
     /*
      * The first character of the address must not be a whitespace,
